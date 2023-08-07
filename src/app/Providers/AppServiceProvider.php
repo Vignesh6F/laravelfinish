75df24e5d8046b212cc5a3c468e82504af09c520
vky <vnobala92@gmail.com>
2023-08-07 17:04:38 +0530

    env change

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -55,9 +55,9 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-		if (env('APP_ENV') === 'production') {
-       		$this->app['request']->server->set('HTTPS', true);
-		}
+	//	if (env('APP_ENV') === 'production') {
+    //   		$this->app['request']->server->set('HTTPS', true);
+	//	}
 		define('EMAIL_LOGO_URL', 'images/logo.png');
 		define('LOGIN_USER_TYPE', request()->segment(1));
 
